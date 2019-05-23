f26cd0089c88f34abdb8e5c8a817c5b335e86133
Sabbir Ahmed <sabbirs93@gmail.com>
2019-05-23 10:14:10 +0600

    Create variables.js

---
+++ variables_scoping/variables.js
@@ -0,0 +1,11 @@
+function setWidth(){
+            var width = 100;
+            console.log(width);
+        }
+        setWidth();
+        var age = 100;
+        if(age>12)
+        {
+            var Years = age * 7;
+            console.log(`You are ${Years} years old.`);
+        } 
